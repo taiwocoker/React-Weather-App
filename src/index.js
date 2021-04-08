86f26e3cdf0356d9c1ab9f6d66a474096050d66f
taiwocoker <taiwofcoker@gmail.com>
2021-04-08 19:00:30 +0100

    Create store for the application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+const initialState = {
+  Loading: false,
+}
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
