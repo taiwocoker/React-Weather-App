5cd7673a90ecbea3c355c005bedd94ecf5dfa6c0
taiwocoker <taiwofcoker@gmail.com>
2021-04-09 22:44:22 +0100

    Build basic weather info ui

--- src/components/weatherInfo.jsx
+++ src/components/weatherInfo.jsx
@@ -1,28 +1,73 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import WeatherDataAction from '../redux/actions/weatherDataActions'
-import LoadingAction from "../redux/actions/loadingActions";
-
+import LoadingAction from '../redux/actions/loadingActions'
 
 const WeatherInfo = () => {
+  const [currentTemp, setCurrentTemp] = useState('fah')
+  const dispatch = useDispatch()
+  const weatherData = useSelector(({ ForecastData, Loading }) => ({
+    ForecastData,
+    Loading,
+  }))
+  useEffect(() => {
+    dispatch(WeatherDataAction())
+    dispatch(LoadingAction(false))
+  }, [])
 
- const dispatch = useDispatch()
- const weatherData = useSelector(({ ForecastData, Loading }) => ({
-   ForecastData,
-   Loading,
- }))
-console.log(weatherData.ForecastData, weatherData.Loading)
- useEffect(() => {
-  dispatch(WeatherDataAction())
-  dispatch(LoadingAction(false))
- }, [])
- if(weatherData.Loading) return <h1>Loading...</h1>
- console.log(weatherData.ForecastData)
- return (
-  <div>
-   Somoye
-  </div>
- )
+  if (weatherData.Loading) return <h1>Loading...</h1>
+
+  return (
+    <>
+      <div>
+        <input
+          type='radio'
+          id='cel'
+          name='temperature'
+          value='cel'
+          onClick={() => setCurrentTemp('cel')}
+        />
+        <label htmlFor='cel'>Celcius</label>
+        <br />
+        <input
+          type='radio'
+          id='fah'
+          name='temperature'
+          value='fah'
+          onClick={() => setCurrentTemp('fah')}
+        />
+        <label htmlFor='fah'>Fahrenheit</label>
+        <br />
+      </div>
+      <section className='d-flex justify-content-between'>
+        {weatherData.ForecastData.map((data, index) => {
+         const date = new Date(data.dt*1000).toDateString()
+         return (
+           <article key={index} className='border '>
+             {currentTemp === 'fah' ? (
+               <>
+                 <p>{`Temp: ${(
+                   ((data.main.temp - 273.15) * 9) / 5 +
+                   32
+                 ).toFixed(1)}F`}</p>
+                 <p>{date}</p>
+                 <p>{data.weather[0].main}</p>
+                 <p>{data.weather[0].description}</p>
+               </>
+             ) : (
+               <>
+                 <small>{`Temp: ${(data.main.temp - 273.15).toFixed(
+                   1
+                 )}C`}</small>
+                 <small>{date}</small>
+               </>
+             )}
+           </article>
+         )
+})}
+      </section>
+    </>
+  )
 }
 
 export default WeatherInfo
