59510b7411d9f1b576d107656576ba95512881eb
taiwocoker <taiwofcoker@gmail.com>
2021-04-10 18:32:30 +0100

    Update weather data actions

--- src/redux/actions/weatherDataActions.js
+++ src/redux/actions/weatherDataActions.js
@@ -1,17 +1,22 @@
 import { toast } from 'react-toastify'
 import * as types from './actionTypes'
 import weatherDataApi from '../../api/weatherDataApi'
+import LoadingAction from "./loadingActions";
+
 export const loadWeatherSuccess = (weatherData) => ({
   type: types.SET_WEATHER_DATA,
   weatherData,
 })
+
 function loadWeatherData(){
   return async (dispatch) => {
     try {
       const data = await weatherDataApi()
       if (data) {
+        dispatch(LoadingAction(false))
         dispatch(loadWeatherSuccess(data.list))
       } else {
+        dispatch(LoadingAction(false))
         dispatch(loadWeatherSuccess({}))
       }
     } catch (error) {
