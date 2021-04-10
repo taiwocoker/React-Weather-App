f0af983ff8fdb16ed9a8cd9424dc82b4cfc05412
taiwocoker <taiwofcoker@gmail.com>
2021-04-10 14:28:06 +0100

    Update weather api

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,5 +1,13 @@
+import { resolveResponse } from '../helpers/groupWeatherData'
+
 export async function handleResponse(response) {
-  if (response.ok) return response.json()
+  if (response.ok) {
+    const data = await response.json()
+    const result = resolveResponse(data)
+    console.log(result)
+
+    return result
+  }
   if (response.status === 400) {
     const error = await response.text()
     throw new Error(error)
--- src/api/weatherDataApi.js
+++ src/api/weatherDataApi.js
@@ -2,7 +2,7 @@ import { handleResponse, handleError } from './apiUtils'
 const weatherUrl =
   'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40'
 
- const getWeatherData = async () =>
+const getWeatherData = async () =>
   fetch(weatherUrl).then(handleResponse).catch(handleError)
 
-export default getWeatherData;
+export default getWeatherData
