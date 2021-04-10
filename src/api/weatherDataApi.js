8fbbd81aae176d99d8b28d8f2f65da160a5710f7
taiwocoker <taiwofcoker@gmail.com>
2021-04-10 09:27:14 +0100

    Update app api

--- src/api/weatherDataApi.js
+++ src/api/weatherDataApi.js
@@ -1,6 +1,6 @@
 import { handleResponse, handleError } from './apiUtils'
 const weatherUrl =
-  'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=5'
+  'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40'
 
  const getWeatherData = async () =>
   fetch(weatherUrl).then(handleResponse).catch(handleError)
