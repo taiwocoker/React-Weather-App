8947285fc5e087e868e1a9f1aa34168da17b5414
taiwocoker <taiwofcoker@gmail.com>
2021-04-11 23:43:27 +0100

    Modify weather actions

--- src/redux/actions/weatherDataActions.js
+++ src/redux/actions/weatherDataActions.js
@@ -1,27 +1,29 @@
 import { toast } from 'react-toastify'
 import * as types from './actionTypes'
 import weatherDataApi from '../../api/weatherDataApi'
-import LoadingAction from "./loadingActions";
+import LoadingAction from './loadingActions'
 
 export const loadWeatherSuccess = (weatherData) => ({
   type: types.SET_WEATHER_DATA,
   weatherData,
 })
 
-function loadWeatherData(){
+function loadWeatherData() {
   return async (dispatch) => {
     try {
       const data = await weatherDataApi()
       if (data) {
-        dispatch(LoadingAction(false))
-        dispatch(loadWeatherSuccess(data.list))
+        let outputData = []
+        for (const i in data) {
+          outputData.push(data[i])
+        }
+        dispatch(loadWeatherSuccess(outputData))
       } else {
-        dispatch(LoadingAction(false))
-        dispatch(loadWeatherSuccess({}))
+        dispatch(loadWeatherSuccess(null))
       }
     } catch (error) {
       toast.error(`Whoops!, ${error.message} occured`)
     }
   }
 }
-export default loadWeatherData;
+export default loadWeatherData
