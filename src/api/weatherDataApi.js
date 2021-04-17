60340cee03f243070aac12741264f92bbf6c28aa
taiwocoker <taiwofcoker@gmail.com>
2021-04-17 22:47:01 +0100

    Fix cross-resource error

--- src/api/weatherDataApi.js
+++ src/api/weatherDataApi.js
@@ -2,7 +2,7 @@ import { handleResponse, handleError } from './apiUtils'
 
 const getWeatherData = async (city) =>
   fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=75f972b80e26f14fe6c920aa6a85ad57&cnt=40`
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=75f972b80e26f14fe6c920aa6a85ad57&cnt=40`
 
   )
     .then(handleResponse)
